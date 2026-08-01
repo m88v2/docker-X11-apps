147b2bce8c5eb09cd07ed14137634cd261c1147b
Fabiano Matos <fabiano.matoz@gmail.com>
2026-08-01 15:28:30 -0300

    keepassxc - update run command

--- keepass-xc/keepassxc.sh
+++ keepass-xc/keepassxc.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #
 # ############################################################################
-# Project: docker-X11-apps (none)
-# File...: remmina/remmina.sh
+# Project: <<myroot>> (<<projectversion>>)                                     #
+# File...: <<relativefile>>                                                    #
 # Created: Tuesday, 2023/01/17 - 02:57:16
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Thursday, 2023/01/26 - 20:20:44
-# Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
+# Last Modified: Saturday, 2026/08/01 - 15:28:08                               #
+# Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)                            #
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.1.9
+# Version: <<myfileversion>>                                                   #
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  >
@@ -18,9 +18,8 @@
 #
 docker run --rm \
     -it \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v $(pwd):/home/user/share \
-	-v $(pwd)/config:/home/user/${XDG_DATA_HOME}/remmina \
-	-e DISPLAY=$DISPLAY \
-    --name remmina-docker \
-	m88v2/remmina
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $(pwd):/home/user/share \
+    -e DISPLAY=$DISPLAY \
+    --name keepassxc-docker \
+    m88v2/keepassxc
